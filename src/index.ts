15861b453df3bbeec01dfb5d65e17fa39a55e94b
Jeremiah Wagstaff <jeremiah.wagstaff@gmail.com>
2019-12-11 10:54:19 -0800

    fix write destination

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
 // tslint:disable: no-console
 import * as fs from 'fs';
+import * as path from 'path';
 import * as crypto from './crypto';
 
 const tests = 16;
-const samples = 10;
+const samples = 100;
 const times: number[][] = [];
 const piece = crypto.randomBytes(4096);
 const key = crypto.randomBytes(32);
 const index = 12312343;
 
+// Test Encode/Decode time for AES-256 within 128 round increments
 for (let r = 128; r <= tests * 128; r += 128) {
   let totalEncodeTime = 0;
   let totalDecodeTime = 0;
 
+  // Test each increment over many samples, taking the mean
   for (let s = 0; s < samples; ++s) {
     const encodeStart = Date.now();
     const encoding = crypto.encode(piece, index, key, r);
@@ -32,14 +35,14 @@ for (let r = 128; r <= tests * 128; r += 128) {
   times.push([averageEncodeTime, averageDecodeTime]);
 }
 
+// output the collated results to console and csv
 let csvContent = "";
 
 for (let t = 0; t < tests; ++t) {
   console.log(`\nFor ${(t + 1) * 128} rounds:`);
   console.log(`Average encoding time is ${times[t][0]} ms`);
   console.log(`Average decoding time is ${times[t][1]} ms`);
-
   csvContent += `${times[t][0]},${times[t][1]}\r\n`;
 }
 
-fs.writeFileSync('benchmarks.csv', csvContent);
+fs.writeFileSync(path.normalize('../results/encode-decode.csv'), csvContent);
